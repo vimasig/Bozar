47fdb69c8c79bbdedfa9dff13dce7ebbca4bf512
vimasig <70583277+vimasig@users.noreply.github.com>
2021-02-11 20:16:49 +0300

    Remove unused import

--- src/main/java/com/vimasig/bozar/obfuscator/Bozar.java
+++ src/main/java/com/vimasig/bozar/obfuscator/Bozar.java
@@ -14,7 +14,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
