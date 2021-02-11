f97d302f820ad3443dab09d61ca9d3a615417c26
vimasig <70583277+vimasig@users.noreply.github.com>
2021-02-11 08:35:33 +0300

    Get version string directly from pom

--- src/main/java/com/vimasig/bozar/obfuscator/utils/BozarUtils.java
+++ src/main/java/com/vimasig/bozar/obfuscator/utils/BozarUtils.java
@@ -1,9 +1,18 @@
 package com.vimasig.bozar.obfuscator.utils;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class BozarUtils {
 
     public static String getVersion() {
-        // TODO: Get version string directly from pom
-        return "1.0.0";
+        try {
+            final Properties properties = new Properties();
+            properties.load(BozarUtils.class.getResourceAsStream("/bozar.properties"));
+            return properties.getProperty("bozar.version");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Unknown version";
+        }
     }
 }
