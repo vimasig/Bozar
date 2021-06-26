8d34b0ee22d3a8d681ebc8130f8ad82001bc6035
vimasig <70583277+vimasig@users.noreply.github.com>
2021-06-26 22:18:07 +0300

    Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module io.github.vimasig.bozar {
+    requires com.google.gson;
+    requires commons.cli;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.base;
+    requires java.desktop;
+    requires java.net.http;
+    requires java.scripting;
+    requires jdk.crypto.ec;
+    requires org.objectweb.asm.commons;
+    requires org.objectweb.asm.util;
+
+    opens io.github.vimasig.bozar.obfuscator.utils.model;
+    opens io.github.vimasig.bozar.ui;
+}
