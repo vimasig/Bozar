cdf95af228075ba88f674236e5140835fac615f8
vimasig <70583277+vimasig@users.noreply.github.com>
2021-08-01 17:38:22 +0300

    Add base methods to ClassTransformer

--- src/main/java/io/github/vimasig/bozar/obfuscator/transformer/ClassTransformer.java
+++ src/main/java/io/github/vimasig/bozar/obfuscator/transformer/ClassTransformer.java
@@ -1,12 +1,15 @@
 package io.github.vimasig.bozar.obfuscator.transformer;
 
 import io.github.vimasig.bozar.obfuscator.Bozar;
+import io.github.vimasig.bozar.obfuscator.utils.ASMUtils;
 import io.github.vimasig.bozar.obfuscator.utils.model.ResourceWrapper;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldNode;
 import org.objectweb.asm.tree.MethodNode;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.jar.JarOutputStream;
 
@@ -40,5 +43,28 @@ public class ClassTransformer implements Opcodes {
 
     public final String getName() {
         return this.getClass().getSimpleName();
+    }
+
+    protected Class<?> getClass(ClassNode classNode) throws ClassNotFoundException {
+        return this.getBozar().getClassLoader().loadClass(ASMUtils.getName(classNode));
+    }
+
+    protected List<ClassNode> getSuperHierarchy(ClassNode base) {
+        var superList = new ArrayList<ClassNode>();
+        while (base != null) {
+            superList.add(base);
+            final String superName = base.superName;
+            base = this.getBozar().getClasses().stream().filter(classNode -> classNode.name.equals(superName)).findFirst().orElse(null);
+        } return superList;
+    }
+
+    protected static List<Class<?>> getSuperHierarchy(Class<?> base, Class<?> to) {
+        var superList = new ArrayList<Class<?>>();
+        while (base != null) {
+            if(to.equals(Object.class) && base.equals(Object.class)) break;
+            superList.add(base);
+            if(base.equals(to)) break;
+            base = base.getSuperclass();
+        } return superList;
     }
 }
