c5e32dfd0f23339abd3dd6ecc4684a2d0c460f67
vimasig <70583277+vimasig@users.noreply.github.com>
2021-08-01 17:32:38 +0300

    Add/edit renamer base methods

--- src/main/java/io/github/vimasig/bozar/obfuscator/transformer/RenamerTransformer.java
+++ src/main/java/io/github/vimasig/bozar/obfuscator/transformer/RenamerTransformer.java
@@ -1,9 +1,13 @@
 package io.github.vimasig.bozar.obfuscator.transformer;
 
 import io.github.vimasig.bozar.obfuscator.Bozar;
+import io.github.vimasig.bozar.obfuscator.utils.ASMUtils;
 import io.github.vimasig.bozar.obfuscator.utils.StringUtils;
+import org.objectweb.asm.tree.ClassNode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class RenamerTransformer extends ClassTransformer {
 
@@ -14,12 +18,22 @@ public class RenamerTransformer extends ClassTransformer {
     protected final HashMap<String, String> map = new HashMap<>();
     protected int index = 0;
 
-    protected void registerMap(String key) {
-        map.put(key, switch (this.getBozar().getConfig().getOptions().getRename()) {
+    protected String registerMap(String key) {
+        var str = switch (this.getBozar().getConfig().getOptions().getRename()) {
             case ALPHABET -> StringUtils.getAlphabetCombinations().get(index);
             case INVISIBLE -> String.valueOf((char)(index + '\u3050'));
             default -> throw new IllegalStateException("transformClass called while rename is disabled, this shouldn't happen");
-        }); index++;
+        };
+        map.put(key, str); index++;
+        return str;
+    }
+
+    protected boolean isMapRegistered(String key) {
+        return map.get(key) != null;
+    }
+
+    protected void registerMap(String key, String value) {
+        map.put(key, value);
     }
 
     public HashMap<String, String> getMap() {
